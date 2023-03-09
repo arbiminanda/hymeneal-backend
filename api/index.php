5b047880fdb6adb6e9cb954af267201ba0b2ab68
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-03-09 07:37:09 +0700

    add allow origin to prevent CORS error

--- api/index.php
+++ api/index.php
@@ -8,6 +8,8 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../common/config/bootstrap.php';
 require __DIR__ . '/config/bootstrap.php';
 
+header('access-control-allow-origin: https://hymeneal-frontend.vercel.app');
+
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../common/config/main.php',
     require __DIR__ . '/../common/config/main-local.php',
